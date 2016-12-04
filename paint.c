b6f5c67b5c83f6ec9e1f854698efcb9596205355
Mamkh27 <mkheer@ucdavis.edu>
2016-12-04 12:30:28 -0800

    Update paint.c

--- paint.c
+++ paint.c
@@ -4,6 +4,7 @@
 
 char** makeCanvas (int numRows, int numCols);
 void printCanvas(int numRows, int numCols, char** canvas);
+bool checkInt(char string[]);
 
 
 void setUp (char*** canvas, int numRows, int numCols){
@@ -46,20 +47,62 @@ return canvas;
 
 		 
 		 
+
+
 int main(int argc, char* argv[]){
-	int numRows;
-	int numCols;
+	int numRows = 0;
+	int numCols = 0;
 	
-	if (argc < 2){
+if (argc < 2){
 		numRows = 10; 
 		numCols = 10; 
 	} else {
 		numRows = atoi(argv[1]);
 		numCols = atoi(argv[2]);
-	} 
+		
+		 if (checkInt(argv[1]) == false){
+			 printf("The number of rows is not an integer.\n");
+			 numRows = 10; 
+			numCols = 10;
+			printf("Making default board of 10 X 10.\n");
+		 }
+		 
+		 if (checkInt(argv[2]) == false){
+			 printf("The number of columns is not an integer.\n");
+			 numRows = 10; 
+			numCols = 10;
+			printf("Making default board of 10 X 10.\n");
+		 }
+		
+		if (numRows < 1){
+			printf("The number of rows is less than 1.\n");
+			numRows = 10; 
+			numCols = 10;		
+			printf("Making default board of 10 X 10.\n");
+		}
+		
+		if (numCols < 1){
+			printf("The number of columns is less than 1.\n");
+			numRows = 10; 
+			numCols = 10;	
+			printf("Making default board of 10 X 10.\n");
+		}
+	}
+	
+	printf(" The board is %d X %d\n", numRows, numCols);
+	return 0;
+}
 
-	 
-	char** canvas; 
+	bool checkInt(char string[]){
+		int i; 
+	
+		for(i = 0; i < strlen(string); i++){
+			if (!(isdigit(string[i]))){
+		return false; 
+			}	
+		}
+		return true; 
+	}
 	
 	setUp(&canvas, numRows, numCols);
 	printCanvas(numRows, numCols, canvas);
